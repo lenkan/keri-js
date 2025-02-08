932afa2e168fb45d0135083d39739cd0b81e1350
lenkan <dlenksjo@gmail.com>
2025-02-08 15:44:28 +0100

    fix keri export

--- packages/keri/src/main-common.ts
+++ packages/keri/src/main-common.ts
@@ -1,10 +1,12 @@
 import cesr from "./parser/cesr-encoding.ts";
-import * as keri from "./events/main.ts";
-export type * from "./events/main.ts";
+import { incept, interact, reply } from "./events/main.ts";
+export * from "./events/main.ts";
 export * from "./keystore/keystore.ts";
 export * from "./parser/codes.ts";
 export * from "./parser/base64.ts";
 export * from "./parser/parser.ts";
 export * from "./keri/habitat.ts";
 
-export { keri, cesr };
+export const keri = { incept, interact, reply };
+
+export { cesr };
