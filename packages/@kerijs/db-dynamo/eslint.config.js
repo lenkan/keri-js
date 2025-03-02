8fcfc54c0aa3035acefff8d2ee26d99fe7ba23f8
lenkan <dlenksjo@gmail.com>
2025-03-02 14:06:08 +0100

    fix linting config

---
+++ packages/@kerijs/db-dynamo/eslint.config.js
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  { ignores: ["node_modules", "dist"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    extends: [pluginJs.configs.recommended, tseslint.configs.strict, tseslint.configs.stylistic],
+    languageOptions: { globals: globals.node },
+  },
+  { rules: prettier.rules },
+  {
+    rules: {
+      // Rules for --experimental-strip-types to work
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/parameter-properties": "error",
+    },
+  },
+);
